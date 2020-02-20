2a0d33a2d336b626f08f8ba1d55825b4ee5ff075
801 <801@801-PC>
2020-02-20 15:52:33 +0800

    454

--- DUANZU-V1.0/src/main/java/com/tedu/duanzu/entity/BillItem.java
+++ DUANZU-V1.0/src/main/java/com/tedu/duanzu/entity/BillItem.java
@@ -18,6 +18,7 @@ public class BillItem {
 	private Integer status;
 	// 
 	
+	
 	@JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone="GMT+8")
 	private Date arrivalDate;
 	private Integer landlordId;
